431e0bd7e61713ee3b95178b36e69ecc6870e734
scummins78 <shaun.cummins@gmail.com>
2014-10-29 20:36:47 -0400

    added framework for getting wiki article data

---
+++ wiki.repository.test/WikiRepositoryTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wiki.Repository;
+
+namespace Wiki.Repository.Test
+{
+    /// <summary>
+    /// Summary description for JustMockTest
+    /// </summary>
+    [TestClass]
+    public class WikiRepositoryTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void GetRandomArticle_Test()
+        {
+            // arrange
+            var repository = new WikiRepository();
+
+            // act
+            var article = repository.GetRandomArticle();
+
+            // assert
+            Assert.IsNotNull(article);
+        }
+    }
+}
