e4ce7dd53c46c887667eea8b747b3fc9bb605d7e
scummins78 <shaun.cummins@gmail.com>
2014-11-04 08:06:19 -0500

    adding missing wiki repository project

---
+++ wiki.repository/Models/WikiArticle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiki.Repository.Models
+{
+    public class WikiArticle
+    {
+        public int pageid { get; set; }
+        public int ns { get; set; }
+        public string title { get; set; }
+        public string extract { get; set; }
+        public string contentmodel { get; set; }
+        public string pagelanguage { get; set; }
+        public DateTime touched { get; set; }
+        public ulong lastrevid { get; set; }
+    }
+}
---
+++ wiki.repository/WikiRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using Wiki.Repository.Models;
+
+namespace Wiki.Repository
+{
+    public class WikiRepository
+    {
+        private const string wikiApiUrl = "http://en.wikipedia.org/w/api.php?action=query&generator=random&prop=extracts|info&format=json";
+
+        /// <summary>
+        /// retrieves a random wikipedia article
+        /// </summary>
+        /// <returns>random article</returns>
+        public WikiArticle GetRandomArticle()
+        {
+            WikiArticle article = null;
+            string wikiData = GetWikiData(wikiApiUrl);
+            dynamic wikiObject = JsonConvert.DeserializeObject(wikiData);
+
+            foreach(var item in wikiObject.query.pages)
+            {
+                // this will only occur once;  need a better method
+                article = new JavaScriptSerializer().Deserialize<WikiArticle>(item.Value.ToString());
+            }
+            
+            return article;
+        }
+
+
+        /// <summary>
+        /// retrieve json data from web service endpoint
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string GetWikiData(string url)
+        {
+            string json = string.Empty;
+
+            using (var client = new WebClient())
+            {
+                json = client.DownloadString(url);
+            }
+
+            return json;
+        }
+ 
+
+    }
+}
